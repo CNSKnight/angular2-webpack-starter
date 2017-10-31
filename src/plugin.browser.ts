478b45e67d599d07c86ad67adf8f2aec50fb34d4
Codename: Steeve Knight <steeve@dharmiweb.net>
2017-10-31 09:31:14 -0400

    trunk-style rollup;

--- src/plugin.browser.ts
+++ src/plugin.browser.ts
@@ -1,25 +1,46 @@
-/*
- * Angular bootstraping
+/**
+ * Angular bootstrapping
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { decorateModuleRef } from './app/environment';
-import { bootloader } from '@angularclass/hmr';
-/*
+import { hmrModule  } from '@angularclass/hmr';
+
+/**
  * App Module
  * our top level module that holds all of our components
  */
 import { PluginModule } from './app'; // ie ./app/index.js
 
-/*
+/**
  * Bootstrap our Angular app with a top level NgModule
  */
 export function plugin(): Promise<any> {
   return platformBrowserDynamic()
     .bootstrapModule(PluginModule)
     .then(decorateModuleRef)
+    .then((ngModuleRef: any) => {
+      // `module` global ref for webpackhmr
+      // Don't run this in Prod
+      return hmrModule(ngModuleRef, module);
+    })
     .catch((err) => console.error(err));
 }
 
-// needed for hmr
-// in prod this is replace for document ready
-bootloader(plugin);
+/**
+ * Needed for hmr
+ * in prod this is replace for document ready
+ */
+switch (document.readyState) {
+  case 'loading':
+    document.addEventListener('DOMContentLoaded', _domReadyHandler, false);
+    break;
+  case 'interactive':
+  case 'complete':
+  default:
+    plugin();
+}
+
+function _domReadyHandler() {
+ document.removeEventListener('DOMContentLoaded', _domReadyHandler, false);
+ plugin();
+}
